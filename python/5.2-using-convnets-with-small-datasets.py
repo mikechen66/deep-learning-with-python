4b776926edeb46222cee0b6abee32d6d2fa086a3
mikechen66 <875661606@qq.com>
2022-11-03 09:55:04 +0800

    Add files via upload

---
+++ python/5.2-using-convnets-with-small-datasets.py
@@ -0,0 +1,362 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 5.2 - Using convnets with small datasets
+# 
+# The code contains the code sample found in Chapter 5, Section 2 of [Deep Learning with Python]
+# (https://www.manning.com/books/deep-learning-with-python?a_aid=keras&a_bid=76564dff). Note that 
+# the original text features far more content, in particular further explanations and figures: in 
+# this notebook, you will only find source code and related comments.
+# 
+# # Training a convnet from scratch on a small dataset
+# 
+# Having "few" samples can mean anywhere from a few hundreds to a few tens of thousands of images. 
+# As a practical example, we will focus on classifying images as "dogs" or "cats", in a dataset 
+# containing 4000 pictures of cats and dogs (2000 cats, 2000 dogs). We will use 2000 pictures for 
+# training, 1000 for validation, and finally 1000 for testing.
+# 
+# Techniques for applying deep learning to small datasets: 
+# 
+# Conduct feature extraction with a pre-trained network,  training a small model from scratch, doing 
+# feature extracting using a pre-trained model, and fine-tuning a pre-trained model. 
+# 
+# Downloading the data
+# 
+# You can download the original dataset at: https://www.kaggle.com/c/dogs-vs-cats/data` with your
+# Kaggle account. 
+
+
+import keras
+keras.__version__
+
+import tensorflow as tf 
+import os, shutil
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+from numba import cuda
+
+
+# Set up the GPU growth to avoid a sudden stop of the runtime with the reminding message: Could not 
+# create cuDNN handle.
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+# The path to the directory where the original dataset was uncompressed
+original_dataset_dir = '/home/mic/datasets/kaggle_original_data'
+
+# The directory where we will store our smaller dataset
+base_dir = '/home/mic/Documents/datasets/cats_and_dogs_small'
+os.mkdir(base_dir)
+
+# Directories for our training, validation and test splits
+train_dir = os.path.join(base_dir, 'train')
+os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+os.mkdir(test_dir)
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+os.mkdir(train_cats_dir)
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+os.mkdir(train_dogs_dir)
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+os.mkdir(validation_cats_dir)
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+os.mkdir(validation_dogs_dir)
+
+# Directory with our validation cat pictures
+test_cats_dir = os.path.join(test_dir, 'cats')
+os.mkdir(test_cats_dir)
+
+# Directory with our validation dog pictures
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+os.mkdir(test_dogs_dir)
+
+# Copy first 1000 cat images to train_cats_dir
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy next 500 cat images to validation_cats_dir
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_cats_dir, fname)
+    shutil.copyfile(src, dst)
+    
+# Copy next 500 cat images to test_cats_dir
+fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_cats_dir, fname)
+    shutil.copyfile(src, dst)
+    
+# Copy first 1000 dog images to train_dogs_dir
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+    
+# Copy next 500 dog images to validation_dogs_dir
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+    
+# Copy next 500 dog images to test_dogs_dir
+fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+# As a sanity check —— count how many pictures we have in each training split (train/validation/test):
+
+print('total training cat images:', len(os.listdir(train_cats_dir)))
+print('total training dog images:', len(os.listdir(train_dogs_dir)))
+print('total validation cat images:', len(os.listdir(validation_cats_dir)))
+print('total validation dog images:', len(os.listdir(validation_dogs_dir)))
+print('total test cat images:', len(os.listdir(test_cats_dir)))
+print('total test dog images:', len(os.listdir(test_dogs_dir)))
+
+
+# Building our network
+
+# Note that the depth of the feature maps is progressively increasing in the network (from 32 to 128), 
+# while the size of the feature maps is decreasing (from 148x148 to 7x7). This is a pattern that you 
+# will see in almost all convnets. 
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+# Let's take a look at how the dimensions of the feature maps change with every successive layer:
+model.summary()
+
+# Since we ended our network with a single sigmoid unit, we will use binary crossentropy as our loss. 
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+
+# Data preprocessing
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        # This is the target directory
+        train_dir,
+        # All images will be resized to 150x150
+        target_size=(150, 150),
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+
+# The generators: it yields batches of 150x150 RGB images (shape `(20, 150, 150, 3)`) and binary labels 
+# (shape `(20,)`). 20 is the number of samples in each batch (the batch size). 
+
+for data_batch, labels_batch in train_generator:
+    print('data batch shape:', data_batch.shape)
+    print('labels batch shape:', labels_batch.shape)
+    break
+
+
+# Fit our model to the data using the `fit_generator` method
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=30,
+      validation_data=validation_generator,
+      validation_steps=50)
+
+
+# It is good practice to always save your models after training:
+model.save('cats_and_dogs_small_1.h5')
+
+
+# Plot the loss and accuracy of the model over the training and validation data during training:
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
+# Using data augmentation
+
+# Conduct a number of random transformations with the ImageDataGenerator instance. 
+datagen = ImageDataGenerator(
+      rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+# This is module with image preprocessing utilities
+from keras.preprocessing import image
+
+fnames = [os.path.join(train_cats_dir, fname) for fname in os.listdir(train_cats_dir)]
+
+# We pick one image to "augment"
+img_path = fnames[3]
+
+# Read the image and resize it
+img = image.load_img(img_path, target_size=(150, 150))
+
+# Convert it to a Numpy array with shape (150, 150, 3)
+x = image.img_to_array(img)
+
+# Reshape it to (1, 150, 150, 3)
+x = x.reshape((1,) + x.shape)
+
+# The .flow() command below generates batches of randomly transformed images.
+# It will loop indefinitely, so we need to `break` the loop at some point!
+i = 0
+for batch in datagen.flow(x, batch_size=1):
+    plt.figure(i)
+    imgplot = plt.imshow(image.array_to_img(batch[0]))
+    i += 1
+    if i % 4 == 0:
+        break
+
+plt.show()
+
+# Perform the model 
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+# Train our network using data augmentation and dropout:
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,)
+
+# Note that the validation data should not be augmented!
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        # This is the target directory
+        train_dir,
+        # All images will be resized to 150x150
+        target_size=(150, 150),
+        batch_size=32,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=100,
+      validation_data=validation_generator,
+      validation_steps=50)
+
+
+# Save our model for convnet visualization.
+model.save('cats_and_dogs_small_2.h5')
+
+
+# Plot our results again:
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
+# Release the GPU memory
+cuda.select_device(0)
+cuda.close()

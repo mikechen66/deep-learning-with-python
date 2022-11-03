f8bbe82ab8079c1b85348d7682cbead76dfd9a53
mikechen66 <875661606@qq.com>
2022-11-03 11:38:32 +0800

    Add files via upload

---
+++ python/6.1-using-word-embeddings.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# Using word embeddings
+# 
+# This notebook contains the second code sample found in Chapter 6, Section 1 of [Deep Learning with Python]
+# (https://www.manning.com/books/deep-learning-with-python?a_aid=keras&a_bid=76564dff). 
+
+# A popular and powerful way to associate a vector with a word is the use of dense word vectors, also called 
+# word embeddings. While the vectors obtained through one-hot encoding are binary, sparse (mostly made of 
+# zeros) and very high-dimensional word embeddingsare low-dimensional floating point vectors (i.e. dense 
+# vectors, as opposed to sparse vectors). 
+
+# Download the word embeddings
+
+# Go to `https://nlp.stanford.edu/projects/glove/` for the 822MB zip file named `glove.6B.zip`, containing 
+# 100-dimensional embedding vectors for 400,000 words (or non-word tokens). Download the aclImdb from 
+# https://ai.stanford.edu/~amaas/data/sentiment/
+
+
+import keras
+keras.__version__
+
+from keras.layers import Embedding
+from keras.datasets import imdb
+from keras import preprocessing
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+import os
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense
+import matplotlib.pyplot as plt
+from numba import cuda
+
+
+# The Embedding layer takes at least two arguments: the number of possible tokens, here 1000 (1 + maximum 
+# word index), and 64 dimensionality of the embeddings.
+embedding_layer = Embedding(1000, 64)
+
+
+# Number of words to consider as features
+max_features = 10000
+# Cut texts after this number of words 
+# (among top max_features most common words)
+maxlen = 20
+
+# Load the data as lists of integers.
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+
+# This turns our lists of integers
+# into a 2D integer tensor of shape `(samples, maxlen)`
+x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
+
+model = Sequential()
+
+# Specify the maximum input length to the Embedding layer. 
+model.add(Embedding(10000, 8, input_length=maxlen))
+
+# Flatten the 3D tensor of embeddings into a 2D tensor of shape `(samples, maxlen * 8)`
+model.add(Flatten())
+
+# Add the classifier on top
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+model.summary()
+
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=32,
+                    validation_split=0.2)
+
+# Download the aclImdb from https://ai.stanford.edu/~amaas/data/sentiment/
+imdb_dir = '/home/mic/datasets/aclImdb'
+train_dir = os.path.join(imdb_dir, 'train')
+
+labels = []
+texts = []
+
+for label_type in ['neg', 'pos']:
+    dir_name = os.path.join(train_dir, label_type)
+    for fname in os.listdir(dir_name):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname))
+            texts.append(f.read())
+            f.close()
+            if label_type == 'neg':
+                labels.append(0)
+            else:
+                labels.append(1)
+
+# Tokenize the data with cutting reviews after 100 words
+maxlen = 100 
+training_samples = 200  # We will be training on 200 samples
+validation_samples = 10000  # We will be validating on 10000 samples
+max_words = 10000  # We will only consider the top 10,000 words in the dataset
+
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+data = pad_sequences(sequences, maxlen=maxlen)
+
+labels = np.asarray(labels)
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
+
+# Split the data into a training set and a validation set
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+
+x_train = data[:training_samples]
+y_train = labels[:training_samples]
+x_val = data[training_samples: training_samples + validation_samples]
+y_val = labels[training_samples: training_samples + validation_samples]
+
+
+# Pre-process the embeddings
+glove_dir = '/home/mic/datasets/glove.6b'
+embeddings_index = {}
+f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'))
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+
+embedding_dim = 100
+
+embedding_matrix = np.zeros((max_words, embedding_dim))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if i < max_words:
+        if embedding_vector is not None:
+            # Words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+
+# Define a model
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+
+
+# Load the GloVe embeddings in the model
+model.layers[0].set_weights([embedding_matrix])
+model.layers[0].trainable = False
+
+
+# Train and evaluate
+
+# Let's compile our model and train it:
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=32,
+                    validation_data=(x_val, y_val))
+model.save_weights('pre_trained_glove_model.h5')
+
+
+# Plot its performance over time:
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=32,
+                    validation_data=(x_val, y_val))
+
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+# Evaluate the model on the test data. First, we will need to tokenize the test data:
+test_dir = os.path.join(imdb_dir, 'test')
+
+labels = []
+texts = []
+
+for label_type in ['neg', 'pos']:
+    dir_name = os.path.join(test_dir, label_type)
+    for fname in sorted(os.listdir(dir_name)):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname))
+            texts.append(f.read())
+            f.close()
+            if label_type == 'neg':
+                labels.append(0)
+            else:
+                labels.append(1)
+
+sequences = tokenizer.texts_to_sequences(texts)
+x_test = pad_sequences(sequences, maxlen=maxlen)
+y_test = np.asarray(labels)
+
+# Load and evaluate the first model
+model.load_weights('pre_trained_glove_model.h5')
+model.evaluate(x_test, y_test)
+
+# Release the GPU memory
+cuda.select_device(0)
+cuda.close()

af82625ccc5bbdb4a32361e48e11bcad68001555
mikechen66 <875661606@qq.com>
2022-11-06 10:18:08 +0800

    Add files via upload

--- python/2.1-a-first-look-at-a-neural-network.py
+++ python/2.1-a-first-look-at-a-neural-network.py
@@ -22,13 +22,13 @@ from numba import cuda
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-# Let's have a look at the training data:
+# Let's have a look at the training data (only for Jupyter Notebook and Termninal Console)
 
 # -train_images.shape
 # -len(train_labels)
 # -train_labels
 
-# Let's have a look at the test data:
+# Let's have a look at the test data((only for Jupyter Notebook and Termninal Console))
 
 # -test_images.shape
 # -len(test_labels)
@@ -62,6 +62,7 @@ test_images = test_images.astype('float32') / 255
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 
+
 # Fit the model to its training data.
 
 network.fit(train_images, train_labels, epochs=5, batch_size=128)
@@ -73,5 +74,6 @@ test_loss, test_acc = network.evaluate(test_images, test_labels)
 print('test_acc:', test_acc)
 
 
+# Release the GPU memory
 cuda.select_device(0)
 cuda.close()

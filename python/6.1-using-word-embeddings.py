883b3eb7b965d8a8666ed4b39c19dfbed4a5fc04
mikechen66 <875661606@qq.com>
2022-11-08 14:55:08 +0800

    Add files via upload

--- python/6.1-using-word-embeddings.py
+++ python/6.1-using-word-embeddings.py
@@ -9,30 +9,26 @@
 
 # A popular and powerful way to associate a vector with a word is the use of dense word vectors, also called 
 # word embeddings. While the vectors obtained through one-hot encoding are binary, sparse (mostly made of 
-# zeros) and very high-dimensional word embeddingsare low-dimensional floating point vectors (i.e. dense 
+# zeros) and very high-dimensional word embeddings are low-dimensional floating point vectors (i.e. dense 
 # vectors, as opposed to sparse vectors). 
 
 # Download the word embeddings
 
-# Go to `https://nlp.stanford.edu/projects/glove/` for the 822MB zip file named `glove.6B.zip`, containing 
-# 100-dimensional embedding vectors for 400,000 words (or non-word tokens). Download the aclImdb from 
-# https://ai.stanford.edu/~amaas/data/sentiment/
+# Go to the weblink of https://nlp.stanford.edu/projects/glove/ for the 822MB zip file named glove.6B.zip, 
+# containing 100-dimensional embedding vectors for 400,000 words (or non-word tokens). Download the aclImdb 
+# from https://ai.stanford.edu/~amaas/data/sentiment/
 
 
 import keras
-keras.__version__
-
 from keras.layers import Embedding
 from keras.datasets import imdb
 from keras import preprocessing
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Embedding, Flatten, Dense
 import os
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Embedding, Flatten, Dense
 import matplotlib.pyplot as plt
 from numba import cuda
 
@@ -44,15 +40,13 @@ embedding_layer = Embedding(1000, 64)
 
 # Number of words to consider as features
 max_features = 10000
-# Cut texts after this number of words 
-# (among top max_features most common words)
+# Cut texts after this number of words (among top max_features most common words)
 maxlen = 20
 
 # Load the data as lists of integers.
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 
-# This turns our lists of integers
-# into a 2D integer tensor of shape `(samples, maxlen)`
+# This turns our lists of integers into a 2D integer tensor of shape (samples, maxlen)
 x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
 
@@ -61,7 +55,7 @@ model = Sequential()
 # Specify the maximum input length to the Embedding layer. 
 model.add(Embedding(10000, 8, input_length=maxlen))
 
-# Flatten the 3D tensor of embeddings into a 2D tensor of shape `(samples, maxlen * 8)`
+# Flatten the 3D tensor of embeddings into a 2D tensor of shape (samples, maxlen * 8)
 model.add(Flatten())
 
 # Add the classifier on top
@@ -73,6 +67,7 @@ history = model.fit(x_train, y_train,
                     epochs=10,
                     batch_size=32,
                     validation_split=0.2)
+
 
 # Download the aclImdb from https://ai.stanford.edu/~amaas/data/sentiment/
 imdb_dir = '/home/mic/datasets/aclImdb'
@@ -124,7 +119,7 @@ x_val = data[training_samples: training_samples + validation_samples]
 y_val = labels[training_samples: training_samples + validation_samples]
 
 
-# Pre-process the embeddings
+# Preprocess the embeddings
 glove_dir = '/home/mic/datasets/glove.6b'
 embeddings_index = {}
 f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'))
@@ -164,7 +159,7 @@ model.layers[0].trainable = False
 
 # Train and evaluate
 
-# Let's compile our model and train it:
+# Compile our model and train it:
 model.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['acc'])

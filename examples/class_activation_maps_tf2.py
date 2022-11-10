e10edb20533ae99d6b877436e0db01d59443650d
mikechen66 <875661606@qq.com>
2022-11-10 16:33:39 +0800

    Add the CAM for TF2.3

--- examples/class_activation_maps_tf2.py
+++ examples/class_activation_maps_tf2.py
@@ -22,10 +22,7 @@ import numpy as np
 import tensorflow as tf
 import cv2
 import matplotlib.pyplot as plt
-
-# pretrained inception model
 import tensorflow.keras.applications.inception_v3 as inception_v3
-
 import tensorflow as tf
 from numba import cuda
 
@@ -36,37 +33,48 @@ for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
 
-# load model
+# Load the model
 model = inception_v3.InceptionV3(weights='imagenet')
 
-# - IMG_NAME = 'meerkat.jpg'
+# Process the image 
 img_name = '/home/mic/Documents/keras_examples/examples/meerkat.jpeg'
 img = plt.imread(img_name)
 
+plt.figure(figsize=(10, 10))
+plt.imshow(img)
+plt.axis('off')
+plt.show()
+
+
+# Process the image 
 x = cv2.resize(img, (299, 299))
 x = np.expand_dims(x, axis=0) 
 x = inception_v3.preprocess_input(x)
 
-preds = model.predict(x)
 
+# Predict the model 
+preds = model.predict(x)
 preds_decoded = inception_v3.decode_predictions(preds)[0]
 
 _, label, conf = preds_decoded[0]
 print("Label: %s with confidence %.2f%%" % (label, conf*100))
 
+
 # OUTPUT: Label meerkat with confidence 93.51%
 
 model.summary()
 
+
+# Conduct the mixed10 layer 
 last_conv_layer = model.get_layer('mixed10')
 
 model_fm = tf.keras.Model(inputs=model.inputs,
-                          outputs=[
-                              model.output,
-                              last_conv_layer.output
-                          ])
+                          outputs=[model.output,
+                                   last_conv_layer.output])
+
 
 model_out, feature_maps = model_fm.predict(x)
+
 
 # get rid of the batch channel, e.g. (1, 1000) -> (1000,)
 feature_maps = np.squeeze(feature_maps)
@@ -90,8 +98,32 @@ print(winning_weights.shape)    # (2048,)
 
 CAM = np.sum(feature_maps * winning_weights, axis=2)
 
+print(CAM.shape)
+plt.imshow(CAM)
+plt.show()
+
 # resize CAM
 heatmap = cv2.resize(CAM, (img.shape[1], img.shape[0]))
+
+# plot heatmap and image side by side
+plt.subplots(1, 2, figsize=(15, 6))
+plt.subplot(1,2,1)
+plt.imshow(heatmap)
+plt.title('Heatmap (resized CAM)')
+plt.subplot(1,2,2)
+plt.imshow(img)
+plt.title('Original image')
+plt.show()
+
+
+# Plot the heatmap again 
+plt.figure(figsize=(10, 10))
+plt.imshow(img, alpha=0.5)
+plt.imshow(heatmap, cmap='jet', alpha=0.5)
+plt.title(f'{label} with confidence {conf*100:.2f}%')
+plt.axis('off')
+#plt.savefig('./images/superimpose.png', dpi=300, bbox_inches='tight')
+plt.show()
 
 
 # Release the GPU memory
